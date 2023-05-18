e576b51e6529250e64c8ff5a10630e7432c0e2c0
Lubega Mark <mark@axual.com>
2023-05-19 01:20:51 +0300

    Make example envs private

--- examples/axual/33_environments.tf
+++ examples/axual/33_environments.tf
@@ -1,9 +1,9 @@
 resource "axual_environment" "test-env" {
   name = "test-env"
-  short_name = "test-env"
+  short_name = "testenv"
   description = "This sis a long descripion"
   color = "#069499"
-  visibility = "Public"
+  visibility = "Private"
   authorization_issuer = "Auto"
   instance = "51be2a6a5eee481198787dc346ab6608"
   owners = "dd84b3ee8e4341fbb58704b18c10ec5c"
@@ -11,10 +11,10 @@ resource "axual_environment" "test-env" {
 
 resource "axual_environment" "test-env2" {
   name = "test-env2"
-  short_name = "test-env2"
+  short_name = "testenv2"
   description = "This sis a long descripion"
   color = "#6532cd"
-  visibility = "Public"
+  visibility = "Private"
   authorization_issuer = "Auto"
   instance = "51be2a6a5eee481198787dc346ab6608"
   owners = "dd84b3ee8e4341fbb58704b18c10ec5c"
