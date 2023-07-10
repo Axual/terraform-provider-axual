b37ceea2b26b6c33d2f6ea3d46a70f499be5e34c
Lubega Mark <mark@axual.com>
2023-07-10 18:33:29 +0300

    Enable deleting all resources except envs

--- examples/axual/33_application_access_grant_approvals.tf
+++ examples/axual/33_application_access_grant_approvals.tf
@@ -6,3 +6,15 @@ resource "axual_application_access_grant_approval" "dash_consume_logs_staging" {
   application_access_grant = axual_application_access_grant.dash_consume_from_logs_in_staging.id
 }
 
+
+resource "axual_application_access_grant_approval" "dash_consume_support_production"{
+  application_access_grant = axual_application_access_grant.dash_consume_from_support_in_production.id
+}
+
+resource "axual_application_access_grant_approval" "log_consume_support_dev"{
+  application_access_grant = axual_application_access_grant.log_scraper_consume_from_support_in_dev.id
+}
+
+resource "axual_application_access_grant_approval" "dash_consume_logs_production"{
+  application_access_grant = axual_application_access_grant.dash_consume_from_logs_in_production.id
+}
