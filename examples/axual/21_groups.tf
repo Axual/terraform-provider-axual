a7b91fd5ed6e8adc144ce69b39bbff73622a124e
jumanne <joris.meijer@axual.com>
2023-07-17 16:29:33 +0200

    Added full-example which shows the capabilities of the current (latest) version of the TerraForm provider for Axual.

--- examples/axual/21_groups.tf
+++
@@ -1,9 +0,0 @@
-resource "axual_group" "developers" {
-  name          = "Developers"
-  phone_number="+37253412559"
-  email_address="gitops.test@axual.com"
-  members       = [
-    axual_user.jane.id,
-    axual_user.john.id,
-  ]
-}
--- examples/axual/33_application_access_grant_rejections.tf
+++
@@ -1,4 +0,0 @@
-resource "axual_application_access_grant_rejection" "scraper_produce_logs_staging_rejection" {
-  application_access_grant = axual_application_access_grant.scraper_produce_to_logs_in_staging.id
-}
-
