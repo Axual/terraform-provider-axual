51aa98eda17b5c61c2df21ad490748ba87244daa
Yunus Sezgin <yunus.sezgin@axual.com>
2023-09-19 11:48:09 +0200

    Rollback the provider.tf

--- examples/axual/provider.tf
+++ examples/axual/provider.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     axual = {
-      source  = "axual.com/hackz/axual"
+      source = "Axual/axual"
+      version = "1.1.0"
     }
   }
 }
@@ -15,7 +16,7 @@ provider "axual" {
   apiurl   = "https://platform.local/api"
   realm    = "axual"
   username = "kubernetes@axual.com" #- or set using env property export AXUAL_AUTH_USERNAME=
-  password = "02480248" #- or set using env property export AXUAL_AUTH_PASSWORD=
+  password = "PLEASE_CHANGE_PASSWORD" #- or set using env property export AXUAL_AUTH_PASSWORD=
   clientid = "self-service"
   authurl = "https://platform.local/auth/realms/axual/protocol/openid-connect/token"
   scopes = ["openid", "profile", "email"]
