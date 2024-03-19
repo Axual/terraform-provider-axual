f5461687b583dc3fe18f414871b608b6195951df
Daniel Akporhonor <daniel.akporhonor@axual.com>
2024-03-19 10:35:48 +0100

    switch to getApplicationsByAttributes

--- axual-webclient/applications.go
+++ axual-webclient/applications.go
@@ -51,11 +51,16 @@ func (c *Client) CreateApplication(data ApplicationRequest) (*ApplicationRespons
 	}
 	return &o, nil
 }
-func (c *Client) GetApplicationByName(name string) (*ApplicationByNameResponse, error) {
-	o := ApplicationByNameResponse{}
-	err := c.RequestAndMap("GET", fmt.Sprintf("%s/applications/search/findByAttributes?name=%s", c.ApiURL, url.QueryEscape(name)), nil, nil, &o)
-	if err != nil {
-		return nil, err
-	}
-	return &o, nil
+
+
+func (c *Client) GetApplicationsByAttributes(attributes url.Values) (*ApplicationsByAttributesResponse, error) {
+    o := ApplicationsByAttributesResponse{}
+
+    url := fmt.Sprintf("%s/applications/search/findByAttributes?%s", c.ApiURL, attributes.Encode())
+	fmt.Println("URL", url)
+    err := c.RequestAndMap("GET", url, nil, nil, &o)
+    if err != nil {
+        return nil, err
+    }
+    return &o, nil
 }
