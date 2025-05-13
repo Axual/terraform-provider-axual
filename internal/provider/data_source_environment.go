c15b317a8675badd002411216b4c3a47ffd72ca1
Bhuvaneswari Balaji <bhuvaneswari@axual.com>
2025-05-13 09:29:39 +0200

    Updated regex patterns of name and shortName instance, app and env

--- internal/provider/data_source_environment.go
+++ internal/provider/data_source_environment.go
@@ -60,7 +60,7 @@ func (d *environmentDataSource) Schema(ctx context.Context, req datasource.Schem
 				Computed:            true,
 				Validators: []validator.String{
 					stringvalidator.LengthBetween(3, 50),
-					stringvalidator.RegexMatches(regexp.MustCompile(`^[a-z0-9._\- ]+$`), "can only contain letters, numbers, dots, dashes and underscores and cannot begin with an underscore, dot or dash"),
+					stringvalidator.RegexMatches(regexp.MustCompile(`(?i)^[a-z0-9._\- ]+$`), "can only contain letters, numbers, dots, dashes and underscores and cannot begin with an underscore, dot or dash"),
 				},
 			},
 			"short_name": schema.StringAttribute{
