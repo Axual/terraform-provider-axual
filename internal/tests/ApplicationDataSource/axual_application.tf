930e98cb1b5d594bd7fbc26f6069cd439e52e148
Ehirim Emeka <emeka@axual.com>
2025-08-25 08:13:30 +0100

    revert axual_application.tf

--- internal/tests/ApplicationDataSource/axual_application.tf
+++ internal/tests/ApplicationDataSource/axual_application.tf
@@ -1,23 +1,23 @@
-# resource "axual_application" "tf-test-app" {
-#   name             = "tf-test-app"
-#   application_type = "Custom"
-#   short_name       = "tf_test_app_short"
-#   application_id   = "tf.test.app"
-#   owners           = data.axual_group.test_group.id
-#   type             = "Java"
-#   visibility       = "Public"
-#   description      = "Axual's TF Test Application"
-# }
+resource "axual_application" "tf-test-app" {
+  name             = "tf-test-app"
+  application_type = "Custom"
+  short_name       = "tf_test_app_short"
+  application_id   = "tf.test.app"
+  owners           = data.axual_group.test_group.id
+  type             = "Java"
+  visibility       = "Public"
+  description      = "Axual's TF Test Application"
+}
 
 data "axual_application" "tf-test-app-imported-by-name" {
-  name = "tf-test-app"
+  name = axual_application.tf-test-app.name
 }
 
 data "axual_application" "tf-test-app-imported-by-short-name" {
-  short_name = "tf_test_app_short"
+  short_name = axual_application.tf-test-app.short_name
 }
 
 data "axual_application" "tf-test-app-imported-by-short-name-and-name" {
-  name = "tf-test-app"
-  short_name = "tf_test_app_short"
+  name = axual_application.tf-test-app.name
+  short_name = axual_application.tf-test-app.short_name
 }
