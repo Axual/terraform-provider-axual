c6db7b4e133e0d8e16d5bdd74689f8af7f494132
Ehirim Emeka <emeka@axual.com>
2025-08-27 12:55:03 +0100

    revert import order

--- internal/provider/data_source_application.go
+++ internal/provider/data_source_application.go
@@ -4,9 +4,6 @@ import (
 	webclient "axual-webclient"
 	"context"
 	"fmt"
-	"net/url"
-	"regexp"
-
 	"github.com/hashicorp/terraform-plugin-framework-validators/datasourcevalidator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
@@ -14,6 +11,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"net/url"
+	"regexp"
 )
 
 var _ datasource.DataSource = &applicationDataSource{}
