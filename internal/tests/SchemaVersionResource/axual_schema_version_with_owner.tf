86a7e89c18baf2b731b0d73720623d9b654bc12f
Dario Benvenuti <dario.benvenuti@axual.com>
2025-10-10 15:28:54 +0200

    refactor: update AVRO schema file names for consistency and align test configurations
    - Renamed AVRO schema files to ensure consistent naming convention.
    - Updated references in schema version test configurations and test cases to match new file names.

--- internal/tests/SchemaVersionResource/axual_schema_version_with_owner.tf
+++ internal/tests/SchemaVersionResource/axual_schema_version_with_owner.tf
@@ -1,5 +1,5 @@
 resource "axual_schema_version" "test_v2_with_owner" {
-  body        = file("avro-schemas/gitops_test_v2.avsc")
+  body        = file("avro-schemas/gitops_test_2_v1.avsc")
   version     = "1.0.0"
   description = "Gitops test schema version"
   type        = "AVRO"
