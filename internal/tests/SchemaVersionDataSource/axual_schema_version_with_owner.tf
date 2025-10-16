acf860783fb958e555665211e62441eccb533094
Dario Benvenuti <dario.benvenuti@axual.com>
2025-10-16 20:11:23 +0200

    feat(tests): add support for PROTOBUF and JSON_SCHEMA schema types
    - Introduced test schemas `tf-protobuf-test1.proto` and `tf-json-schema-test1.json`.
    - Updated test configurations to validate `axual_schema_version` for PROTOBUF and JSON_SCHEMA types.
    - Enhanced schema version validation with additional test cases for schema properties and owners.

--- internal/tests/SchemaVersionDataSource/axual_schema_version_with_owner.tf
+++ internal/tests/SchemaVersionDataSource/axual_schema_version_with_owner.tf
@@ -9,3 +9,29 @@ data "axual_schema_version" "test_v1_imported" {
   full_name = "io.axual.qa.general.GitOpsTest1"
   version   = axual_schema_version.test_v1.version
 }
+
+resource "axual_schema_version" "protobuf_v1" {
+  body = file("protobuf-schemas/tf-protobuf-test1.proto")
+  version     = "1.0.0"
+  description = "AddressBook schema"
+  owners      = data.axual_group.test_group.id
+  type = "PROTOBUF"
+}
+
+data "axual_schema_version" "protobuf_v1_imported" {
+  full_name = "AddressBook"
+  version   = axual_schema_version.protobuf_v1.version
+}
+
+resource "axual_schema_version" "jsonschema_v1" {
+  body = file("json-schemas/tf-json-schema-test1.json")
+  version     = "1.0.0"
+  description = "Person schema"
+  owners      = data.axual_group.test_group.id
+  type = "JSON_SCHEMA"
+}
+
+data "axual_schema_version" "jsonschema_v1_imported" {
+  full_name = "Person"
+  version   = axual_schema_version.jsonschema_v1.version
+}
