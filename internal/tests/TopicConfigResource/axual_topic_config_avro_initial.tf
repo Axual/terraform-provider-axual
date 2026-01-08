e54112f498104e1506a72303889fd2b1b98800f9
Dario Benvenuti <dario.benvenuti@axual.com>
2026-01-08 16:35:49 +0100

    Add schema version v3 for key schema, update schema fields for backward compatibility, and clean up unused resources for test configuration

--- internal/tests/TopicConfigResource/axual_topic_config_avro_initial.tf
+++ internal/tests/TopicConfigResource/axual_topic_config_avro_initial.tf
@@ -1,39 +1,3 @@
-resource "axual_environment" "tf-test-env" {
-  name                 = "tf-development"
-  short_name           = "tfdev"
-  description          = "This is the development environment"
-  color                = "#19b9be"
-  visibility           = "Public"
-  authorization_issuer = "Auto"
-  instance             = data.axual_instance.test_instance.id
-  owners               = data.axual_group.test_group.id
-}
-
-resource "axual_schema_version" "test_key_v1" {
-  body = file("avro-schemas/avro-schema1.avsc")
-  version     = "1.0.0"
-  description = "Gitops test schema version"
-}
-
-resource "axual_schema_version" "test_key_v2" {
-  body = file("avro-schemas/avro-schema1-v2.avsc")
-  version     = "2.0.0"
-  description = "Gitops test schema version"
-}
-
-resource "axual_schema_version" "test_value_v1" {
-  body = file("avro-schemas/avro-schema2.avsc")
-  version     = "1.0.0"
-  description = "Gitops test schema version"
-}
-
-resource "axual_schema_version" "test_value_v2" {
-  body = file("avro-schemas/avro-schema2-v2.avsc")
-  version     = "2.0.0"
-  description = "Gitops test schema version"
-}
-
-
 resource "axual_topic" "tf-test-topic" {
   name             = "test-topic"
   key_type         = "AVRO"
