f3b8d211603fdfe63f86354442c323870cee1692
Dario Benvenuti <dario.benvenuti@axual.com>
2026-04-08 18:00:02 +0200

    chore(tests): add rotated certificate test case and update configs for `axual_application_principal` resource

--- internal/tests/ApplicationDeploymentResource/axual_application_deployment_cert_rotated.tf
+++ internal/tests/ApplicationDeploymentResource/axual_application_deployment_cert_rotated.tf
@@ -1,8 +1,8 @@
 resource "axual_application_principal" "connector_axual_application_principal" {
   environment = axual_environment.tf-test-env.id
   application = axual_application.tf-test-app.id
-  principal   = file("certs/connector-cert.crt")
-  private_key = file("certs/connector-cert.key")
+  principal   = file("certs/connector_cert_rotated.cer")
+  private_key = file("certs/connector_cert_rotated.key")
 }
 
 resource "axual_application_access_grant" "tf-test-application-access-grant" {
@@ -23,7 +23,26 @@ resource "axual_application_access_grant_approval" "tf-test-application-access-g
 resource "axual_application_deployment" "connector_axual_application_deployment" {
   environment = axual_environment.tf-test-env.id
   application = axual_application.tf-test-app.id
+  configs = {
+    "logger.name"                 = "tflogger",
+    "throughput"                  = "10",
+    "topic"                       = "test-topic",
+    "key.converter"               = "StringConverter",
+    "value.converter"             = "StringConverter",
+    "header.converter"            = "",
+    "config.action.reload"        = "restart",
+    "tasks.max"                   = "2",
+    "errors.log.include.messages" = "false",
+    "errors.log.enable"           = "false",
+    "errors.retry.timeout"        = "0",
+    "errors.retry.delay.max.ms"   = "60000",
+    "errors.tolerance"            = "none",
+    "predicates"                  = "",
+    "topic.creation.groups"       = "",
+    "transforms"                  = ""
+  }
   depends_on = [
     axual_application_access_grant_approval.tf-test-application-access-grant-approval,
   ]
 }
+
