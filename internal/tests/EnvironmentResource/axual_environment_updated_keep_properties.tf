2c536dbb86bfd52bc42d6324d1e15592be589fc5
Dario Benvenuti <dario.benvenuti@axual.com>
2026-06-03 16:23:22 +0200

    test(environment): verify updating non-properties fields preserves properties and settings

---
+++ internal/tests/EnvironmentResource/axual_environment_updated_keep_properties.tf
@@ -0,0 +1,31 @@
+resource "axual_group" "team-integrations" {
+  name          = "testgroup9999"
+  phone_number  = "+6112356789"
+  email_address = "test.user@axual.com"
+  members = [
+    data.axual_user.test_user.id,
+  ]
+}
+
+resource "axual_environment" "tf-test-env" {
+  name                 = "tf-development1"
+  short_name           = "tfdev"
+  description          = "This is the terraform testing environment2"
+  color                = "#21ccd2"
+  visibility           = "Public"
+  authorization_issuer = "Stream owner"
+  instance             = data.axual_instance.test_instance.id
+  owners               = data.axual_group.test_group.id
+  retention_time       = 90000
+  partitions           = 2
+  properties = {
+    propertyKey1 = "propertyValue1",
+    propertyKey2 = "propertyValue2"
+  }
+  settings = {
+    enforceDataMasking = "true",
+  }
+  viewers = [
+    axual_group.team-integrations.id,
+  ]
+}
