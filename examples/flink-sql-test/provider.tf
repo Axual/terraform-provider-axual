35d50af989327d11c9f68a0a74beafc11973d90d
Cemre Tok <cemre.tok@axual.com>
2026-08-28 15:47:41 +0200

    AXPD-11717: Terraform Flink cluster, application control implementation added

--- examples/flink-sql-test/provider.tf
+++ examples/flink-sql-test/provider.tf
@@ -10,17 +10,17 @@ terraform {
 provider "axual" {
   # Configuration options
   # (String) URL that will be used by the client for all resource requests
-  apiurl = "https://self-service.qa.np.westeurope.azure.axual.cloud/api"
+  apiurl = ""
   # (String) Axual realm used for the requests
-  realm = "axual"
+  realm = ""
   # (String) Username for all requests. Will be used to acquire a token. It can be omitted if the environment variable AXUAL_AUTH_USERNAME is used.
-  username = "admin-axual"
+  username = ""
   # (String, Sensitive) Password belonging to the user. It can be omitted if the environment variable AXUAL_AUTH_PASSWORD is used.
-  password = "notsecret"
+  password = ""
   # (String) Client ID to be used for OAUTH
-  clientid = "self-service"
+  clientid = ""
   # (String) Token url
-  authurl = "https://self-service.qa.np.westeurope.azure.axual.cloud/auth/realms/axual/protocol/openid-connect/token"
+  authurl = ""
   # (List of String) OAuth authorization server scopes
   scopes = ["openid", "profile", "email"]
 }
